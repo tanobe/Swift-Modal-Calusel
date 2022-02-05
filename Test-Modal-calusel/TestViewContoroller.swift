40c3ecb83dec899f012e56d5e6acc8c49c804f9c
tanobe <stomp0630@icloud.com>
2022-02-05 14:58:17 +0900

    Change details

---
+++ Test-Modal-calusel/TestViewContoroller.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Test-Modal-calusel
+//
+//  Created by Kai Tanobe on 2021/12/08.
+//
+
+import Foundation
+import UIKit
+
+class TestViewContoroller: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.red
+    }
+}
